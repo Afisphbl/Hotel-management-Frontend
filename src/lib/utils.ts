742174a7f120b86c7e790b52b39ff3c8fe5fe232
Abduselam Seid <abuabdurehman0308@gmail.com>
2026-05-22 06:19:32 +0300

    feat: enhance subscription management with filtering, searching, and CRUD functionalities

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,15 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function formatCurrency(
+  amount: number,
+  currency = "ETB",
+  decimals = 2,
+) {
+  return new Intl.NumberFormat("en-ET", {
+    style: "currency",
+    currency,
+    minimumFractionDigits: decimals,
+  }).format(amount)
+}
